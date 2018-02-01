b6a190b390ce906ae5520c29df2958636a023611
John Murphy <murf2073@gmail.com>
2018-02-01 18:36:18 -0500

    inital round of assessment items added with comments

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementByID("colorchange").addEventListener("click", function(){
+		// add code here to make the background color of titlebox div change
+	})
+})
