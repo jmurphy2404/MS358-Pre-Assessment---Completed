8d348b6fb53ce545978363cfae68bee162c2b85e
John Murphy <murf2073@gmail.com>
2018-02-27 14:57:01 -0500

    inline scripts and deleted main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-	document.getElementById('colorchange').addEventListener("click", function(){
-		// add code here to make the background color of titlebox div change
-		document.getElementById('titlebox').style.backgroundColor = "red";
-	});
-});
